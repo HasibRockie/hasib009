5d4b3763aa39c5dd0f2899825b43784483bbd413
hasib <hasiee8004@gmail.com>
2021-06-24 19:32:21 +0600

    Portfolio 1st

---
+++ index.js
@@ -0,0 +1,160 @@
+var colors = ['#03045E','#023E8A','#0077B6','#faa307','#faa307','#e85d04','#dc2f02','#6a040f','#370617','#283618']
+
+document.getElementById("c01").addEventListener(
+    'mouseover',function(){
+        document.getElementById("i01").style.visibility = 'visible';
+        document.getElementById("i01").style.transition = '300ms';
+        document.getElementById("i01").style.transitionDelay = '500ms';
+        document.getElementById("i01").style.transform = 'translateY(-20px) scale(1.3)';
+        document.getElementById("t01").style.fontSize = '4rem';
+        document.getElementById("t01").style.transform = 'translateY(-20px) translateX(45%)';
+        
+    }
+    
+)
+
+document.getElementById("c01").addEventListener(
+    'mouseout',function(){
+        document.getElementById("i01").style.visibility = 'hidden';
+        document.getElementById("i01").style.transition = '200ms';
+        // document.getElementById("i01").style.transitionDelay = '500ms';
+        document.getElementById("t01").style.fontSize = '10rem';
+        document.getElementById("t01").style.transform = 'translateY(0px) translateX(0%)';
+        document.getElementById("c01").style.backgroundColor = 'rgba(255, 255, 255, 0.1)';
+    }
+)
+
+
+
+// 2nd card 
+
+document.getElementById("c02").addEventListener(
+    'mouseover',function(){
+        document.getElementById("i02").style.visibility = 'visible';
+        document.getElementById("i02").style.transition = '300ms';
+        document.getElementById("i02").style.transitionDelay = '500ms';
+        document.getElementById("i02").style.transform = 'translateY(-20px) scale(1.3)';
+        document.getElementById("t02").style.fontSize = '4rem';
+        document.getElementById("t02").style.transform = 'translateY(-20px) translateX(45%)';
+        for( var i=0;i<=10;i++){
+        
+           
+    }
+    }
+)
+
+document.getElementById("c02").addEventListener(
+    'mouseout',function(){
+        document.getElementById("i02").style.visibility = 'hidden';
+        document.getElementById("i02").style.transition = '200ms';
+        // document.getElementById("i01").style.transitionDelay = '500ms';
+        document.getElementById("t02").style.fontSize = '10rem';
+        document.getElementById("t02").style.transform = 'translateY(0px) translateX(0%)';
+        document.getElementById("c02").style.backgroundColor = 'rgba(255, 255, 255, 0.1)';
+    }
+)
+
+
+// card 03
+
+document.getElementById("c03").addEventListener(
+    'mouseover',function(){
+        document.getElementById("i03").style.visibility = 'visible';
+        document.getElementById("i03").style.transition = '300ms';
+        document.getElementById("i03").style.transitionDelay = '500ms';
+        document.getElementById("i03").style.transform = 'translateY(-20px) scale(1.3)';
+        document.getElementById("t03").style.fontSize = '4rem';
+        document.getElementById("t03").style.transform = 'translateY(-20px) translateX(45%)';
+    3
+    }
+)
+
+document.getElementById("c03").addEventListener(
+    'mouseout',function(){
+        document.getElementById("i03").style.visibility = 'hidden';
+        document.getElementById("i03").style.transition = '200ms';
+        // document.getElementById("i01").style.transitionDelay = '500ms';
+        document.getElementById("t03").style.fontSize = '10rem';
+        document.getElementById("t03").style.transform = 'translateY(0px) translateX(0%)';
+        document.getElementById("c03").style.backgroundColor = 'rgba(255, 255, 255, 0.1)';
+    }
+)
+
+
+//card 04
+
+document.getElementById("c04").addEventListener(
+    'mouseover',function(){
+        document.getElementById("i04").style.visibility = 'visible';
+        document.getElementById("i04").style.transition = '300ms';
+        document.getElementById("i04").style.transitionDelay = '500ms';
+        document.getElementById("i04").style.transform = 'translateY(-20px) scale(1.3)';
+        document.getElementById("t04").style.fontSize = '4rem';
+        document.getElementById("t04").style.transform = 'translateY(-20px) translateX(45%)';
+    3
+    }
+)
+
+document.getElementById("c04").addEventListener(
+    'mouseout',function(){
+        document.getElementById("i04").style.visibility = 'hidden';
+        document.getElementById("i04").style.transition = '200ms';
+        // document.getElementById("i01").style.transitionDelay = '500ms';
+        document.getElementById("t04").style.fontSize = '10rem';
+        document.getElementById("t04").style.transform = 'translateY(0px) translateX(0%)';
+        document.getElementById("c04").style.backgroundColor = 'rgba(255, 255, 255, 0.1)';
+    }
+)
+
+
+//card 05
+
+
+document.getElementById("c05").addEventListener(
+    'mouseover',function(){
+        document.getElementById("i05").style.visibility = 'visible';
+        document.getElementById("i05").style.transition = '300ms';
+        document.getElementById("i05").style.transitionDelay = '500ms';
+        document.getElementById("i05").style.transform = 'translateY(-20px) scale(1.3)';
+        document.getElementById("t05").style.fontSize = '4rem';
+        document.getElementById("t05").style.transform = 'translateY(-20px) translateX(45%)';
+    3
+    }
+)
+
+document.getElementById("c05").addEventListener(
+    'mouseout',function(){
+        document.getElementById("i05").style.visibility = 'hidden';
+        document.getElementById("i05").style.transition = '200ms';
+        // document.getElementById("i01").style.transitionDelay = '500ms';
+        document.getElementById("t05").style.fontSize = '10rem';
+        document.getElementById("t05").style.transform = 'translateY(0px) translateX(0%)';
+        document.getElementById("c05").style.backgroundColor = 'rgba(255, 255, 255, 0.1)';
+    }
+)
+
+
+// card 06
+
+document.getElementById("c06").addEventListener(
+    'mouseover',function(){
+        document.getElementById("i06").style.visibility = 'visible';
+        document.getElementById("i06").style.transition = '300ms';
+        document.getElementById("i06").style.transitionDelay = '500ms';
+        document.getElementById("i06").style.transform = 'translateY(-20px) scale(1.3)';
+        document.getElementById("t06").style.fontSize = '4rem';
+        document.getElementById("t06").style.transform = 'translateY(-20px) translateX(45%)';
+    3
+    }
+)
+
+document.getElementById("c06").addEventListener(
+    'mouseout',function(){
+        document.getElementById("i06").style.visibility = 'hidden';
+        document.getElementById("i06").style.transition = '200ms';
+        // document.getElementById("i01").style.transitionDelay = '500ms';
+        document.getElementById("t06").style.fontSize = '10rem';
+        document.getElementById("t06").style.transform = 'translateY(0px) translateX(0%)';
+        document.getElementById("c06").style.backgroundColor = 'rgba(255, 255, 255, 0.1)';
+    }
+)
